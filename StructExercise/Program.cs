6944a7daf9c57c82c7dec99170d6c41b11e7c63a
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2021-12-06 19:57:45 +0200

    Avrage of trimester
    Avrage of 3 hardcoded grades in a struct

---
+++ StructExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StructExercise
+{
+    class Program
+    {
+        public struct Grades
+        {
+            public int FirstGrade;
+            public int SecondGrade;
+            public int ThirdGrade;
+
+            public Grades(int firstGrade, int secondGrade, int thirdGrade)
+            {
+                FirstGrade = firstGrade;
+                SecondGrade = secondGrade;
+                ThirdGrade = thirdGrade;
+            }
+
+            public void GradeAvrage()
+            {
+                double avrage = (FirstGrade + SecondGrade + ThirdGrade) / 3;
+                Console.WriteLine($"The avrage grade of {FirstGrade},{SecondGrade},{ThirdGrade} is {avrage}");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Grades grades = new Grades(5, 3, 7);
+
+            grades.GradeAvrage();
+        }
+    }
+}
